e542d3a8e0fad469a19403ae3cbe38b1d04743b0
Nikhil Mohanty <69157165+Nikhil15Mohanty@users.noreply.github.com>
2026-05-14 08:03:53 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int t[101][101];
+    int solve(int m, int n, int i , int j) {
+        if (i == m -1 && j == n-1) {
+            return 1;
+        }
+
+        if (t[i][j] != -1) return t[i][j];
+
+        if (i < 0 || i >= m || j < 0 || j >=n) {
+            return 0;
+        }
+
+        int right = solve(m,n,i, j+1);
+        int down = solve(m,n,i+1,j);
+
+        return t[i][j] = right + down;
+    }
+    int uniquePaths(int m, int n) {
+        int i = 0, j = 0;
+        memset(t, -1, sizeof(t));
+        return solve(m,n,i,j);
+    }
+};
