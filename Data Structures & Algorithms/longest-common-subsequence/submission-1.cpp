8887ddc0173eba803b605f83e56998de89c2ed9d
Nikhil Mohanty <69157165+Nikhil15Mohanty@users.noreply.github.com>
2026-05-15 08:23:55 +0530

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int t[1001][1001];
+    int solve(string s1, string s2, int i , int j) {
+        if (i >= s1.length() || j >=s2.length()) return 0;
+        if (t[i][j] != -1) return t[i][j];
+
+        if (s1[i] == s2[j]) return t[i][j] = 1 + solve(s1, s2, i+1, j+1);
+
+        return t[i][j] = max(solve(s1,s2, i+1, j), solve(s1,s2,i,j+1));
+    }
+    int longestCommonSubsequence(string s1, string s2) {
+        memset(t, -1, sizeof(t));
+        return solve(s1,s2,0,0);
+    }
+};
