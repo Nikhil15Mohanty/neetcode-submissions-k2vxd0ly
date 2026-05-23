ad195c34ebff68868f13c300bfd5a79859a6258e
Nikhil Mohanty <69157165+Nikhil15Mohanty@users.noreply.github.com>
2026-05-23 11:55:14 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> result;
+        for (int i = 0; i<size; i++) {
+            int product = 1;
+            for (int j = 0; j<size; j++) {
+                if (nums[i] == nums[j] && i == j) {
+                    continue;
+                }
+                else {
+                    product*= nums[j];
+                }
+            }
+
+            result.push_back(product);
+        }
+
+        return result;
+    }
+};
